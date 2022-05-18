5f6fc10bca16190c53aabf5ec3c6ec124c76cfd7
Yuanzhe-Li <li.yuanzhe@northeastern.edu>
2022-05-17 17:33:37 -0700

    Student Management System

---
+++ students-management/src/main/java/com/example/studentsmanagement/dao/UniversityClassDao.java
@@ -0,0 +1,8 @@
+package com.example.studentsmanagement.dao;
+
+import com.example.studentsmanagement.model.UniversityClass;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UniversityClassDao extends CrudRepository<UniversityClass, Long> {
+
+}
